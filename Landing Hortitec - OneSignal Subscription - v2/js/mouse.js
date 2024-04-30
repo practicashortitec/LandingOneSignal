2c307ca8dbc88453eea359ac172b9a6e9777e7ec
practicashortitec <167749420+practicashortitec@users.noreply.github.com>
2024-04-30 17:39:54 +0200

    Delete Landing Hortitec - OneSignal Subscription - v2 directory

--- Landing Hortitec - OneSignal Subscription - v2/js/mouse.js
+++
@@ -1,9 +0,0 @@
-const root = document.documentElement;
- 
-document.addEventListener('mousemove', evt => {
-    let x = evt.clientX / innerWidth;
-    let y = evt.clientY / innerHeight;
- 
-    root.style.setProperty('--mouse-x', x);
-    root.style.setProperty('--mouse-y', y);
-});
